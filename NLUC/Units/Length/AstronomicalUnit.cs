5dceb423e71608605beb2daa9328ece30b1b39a9
Sam <github@samlord.co.uk>
2025-05-25 23:33:34 +0100

    Add AU and light second

---
+++ NLUC/Units/Length/AstronomicalUnit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLUC.Units.Length
+{
+    internal class Au : Unit
+    {
+        public override Units DerivedUnits => Units.Mile;
+
+        public override Units RootBase => Units.Metre;
+
+        public override string[] Shorthand
+        {
+            get
+            {
+                return new string[]
+                {
+                    "au",
+                    "AU"
+                };
+            }
+        }
+
+        public override IUnit ToSIBase()
+        {
+            return new Metres(Value * 149597870700).ToSIBase();
+        }
+
+        public override double FromRootBaseValue(double value)
+        {
+            return new Metres(0).FromRootBaseValue(value) / 149597870700;
+        }
+
+        public Au(double value) : base(value)
+        {
+
+        }
+    }
+}
---
+++ NLUC/Units/Length/LightSecond.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLUC.Units.Length
+{
+    internal class LightSecond : Unit
+    {
+        public override Units DerivedUnits => Units.Mile;
+
+        public override Units RootBase => Units.Metre;
+
+        public override string[] Shorthand
+        {
+            get
+            {
+                return new string[]
+                {
+                    "light-second",
+                    "light-seconds",
+                    "ls"
+                };
+            }
+        }
+
+        public override IUnit ToSIBase()
+        {
+            return new Metres(Value * 299792458).ToSIBase();
+        }
+
+        public override double FromRootBaseValue(double value)
+        {
+            return new Metres(0).FromRootBaseValue(value) / 299792458;
+        }
+
+        public LightSecond(double value) : base(value)
+        {
+
+        }
+    }
+}
